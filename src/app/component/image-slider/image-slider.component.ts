dbe36719689c717758bc1292088910fccbbc72f5
CocoOreo <songhaoranuestc@163.com>
2019-10-22 16:04:33 +0800

    轮播图组件

--- src/app/component/image-slider/image-slider.component.ts
+++ src/app/component/image-slider/image-slider.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input ,AfterViewInit, Renderer2,ElementRef, ViewChild} from '@angular/core';
 
 
 //若不export img 则在根组件中无法设置
@@ -15,13 +15,34 @@ export interface slider{
   styleUrls: ['./image-slider.component.scss']
 })
 
-export class ImageSliderComponent implements OnInit {
+export class ImageSliderComponent implements OnInit,AfterViewInit {
 
-  constructor() { }
-
+  @ViewChild('slider',{static:true}) slider:ElementRef;
+  constructor(private rd2: Renderer2) { }
+  intervalId:any;
+  selectedIndex:number;
   ngOnInit() {
   }
 
   @Input() sliders:slider[]=[];
+
+  ngAfterViewInit(): void {
+    this.intervalId = setInterval(() => {
+      this.rd2.setProperty(
+        this.slider.nativeElement,
+        'scrollLeft',
+        (this.getIndex(++this.selectedIndex) *
+          this.slider.nativeElement.scrollWidth) /
+          this.sliders.length
+      );
+    },1000);
+  }
+
+  getIndex(index:number){
+    return index>=0? index% this.sliders.length 
+    : this.sliders.length-(Math.abs(index)%this.sliders.length)
+
+
+  }
 
 }
