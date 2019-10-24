600f7d13c9e3a7cc2084a9cead6cc5e59322e4c4
CocoOreo <songhaoranuestc@163.com>
2019-10-24 21:20:12 +0800

    频道栏组件

--- src/app/component/horizontal-grid/horizontal-grid.component.ts
+++ src/app/component/horizontal-grid/horizontal-grid.component.ts
@@ -1,4 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+
+
+export interface channel{
+  id:number;
+  title:string;
+  icon:string;
+  link:string;
+}
+//注意接口属性之间用分号而非逗号
+
 
 @Component({
   selector: 'app-horizontal-grid',
@@ -6,10 +16,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./horizontal-grid.component.scss']
 })
 export class HorizontalGridComponent implements OnInit {
+  barMargin:string = '0';
 
+  @Input() channelList:channel[] = [];
   constructor() { }
+
 
   ngOnInit() {
   }
 
+  handleScroll(ev){
+    this.barMargin = `${(100 * ev.target.scrollLeft) /
+      ev.target.scrollWidth}%`;
+  }
 }
